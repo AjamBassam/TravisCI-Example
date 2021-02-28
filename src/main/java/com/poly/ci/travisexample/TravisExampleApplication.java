31e96ac7d78eccfc3035a15d7b8947c45b6d22fb
bassam.ajam95 <bassam.ajam@polymtl.ca>
2021-02-28 18:26:19 -0500

    create new project

---
+++ src/main/java/com/poly/ci/travisexample/TravisExampleApplication.java
@@ -0,0 +1,13 @@
+package com.poly.ci.travisexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisExampleApplication.class, args);
+	}
+
+}
